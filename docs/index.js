fe94d57dde6496fe6a0b2233b7abc6c70203e500
Derek Hoogewerf <dbhoogewerf@gmail.com>
2023-11-09 03:50:14 +0530

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,9 +1,5 @@
-const navbarToggle = navbar.querySelector('#navbar-toggle');
-let isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
-
-const toggleNavbarVisibility = () => {
-  isNavbarExpanded = !isNavbarExpanded;
-  navbarToggle.setAttribute('aria-expanded', isNavbarExpanded);
-};
-
-navbarToggle.addEventListener('click', toggleNavbarVisibility);
+$(document).ready(function(){
+    $('.toggle').click(function(){
+        $('.menu').toggleClass('active');
+    });
+});
